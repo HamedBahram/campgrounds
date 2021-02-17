338a50edda47b877de8fff0750e0f61ad1016a6c
HamedBahram <64147793+HamedBahram@users.noreply.github.com>
2021-02-16 21:04:09 -0500

    turned helmet back on

--- app.js
+++ app.js
@@ -65,53 +65,53 @@ const sessionConfig = {
 app.use(session(sessionConfig))
 app.use(flash())
 
-// helmet
-// app.use(helmet())
-// const scriptSrcUrls = [
-//     'https://stackpath.bootstrapcdn.com/',
-//     'https://api.tiles.mapbox.com/',
-//     'https://api.mapbox.com/',
-//     'https://kit.fontawesome.com/',
-//     'https://cdnjs.cloudflare.com/',
-//     'https://cdn.jsdelivr.net',
-// ]
-// const styleSrcUrls = [
-//     'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css',
-//     'https://kit-free.fontawesome.com/',
-//     'https://stackpath.bootstrapcdn.com/',
-//     'https://api.mapbox.com/',
-//     'https://api.tiles.mapbox.com/',
-//     'https://fonts.googleapis.com/',
-//     'https://use.fontawesome.com/',
-// ]
-// const connectSrcUrls = [
-//     'https://api.mapbox.com/',
-//     'https://a.tiles.mapbox.com/',
-//     'https://b.tiles.mapbox.com/',
-//     'https://events.mapbox.com/',
-// ]
-// const fontSrcUrls = []
-// app.use(
-//     helmet.contentSecurityPolicy({
-//         directives: {
-//             defaultSrc: [],
-//             connectSrc: ["'self'", ...connectSrcUrls],
-//             scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
-//             styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
-//             workerSrc: ["'self'", 'blob:'],
-//             objectSrc: [],
-//             imgSrc: [
-//                 "'self'",
-//                 'blob:',
-//                 'data:',
-//                 'https://res.cloudinary.com/dtijo8xha/',
-//                 'https://images.unsplash.com/',
-//                 'https://source.unsplash.com/',
-//             ],
-//             fontSrc: ["'self'", ...fontSrcUrls],
-//         },
-//     })
-// )
+helmet
+app.use(helmet())
+const scriptSrcUrls = [
+    'https://stackpath.bootstrapcdn.com/',
+    'https://api.tiles.mapbox.com/',
+    'https://api.mapbox.com/',
+    'https://kit.fontawesome.com/',
+    'https://cdnjs.cloudflare.com/',
+    'https://cdn.jsdelivr.net',
+]
+const styleSrcUrls = [
+    'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css',
+    'https://kit-free.fontawesome.com/',
+    'https://stackpath.bootstrapcdn.com/',
+    'https://api.mapbox.com/',
+    'https://api.tiles.mapbox.com/',
+    'https://fonts.googleapis.com/',
+    'https://use.fontawesome.com/',
+]
+const connectSrcUrls = [
+    'https://api.mapbox.com/',
+    'https://a.tiles.mapbox.com/',
+    'https://b.tiles.mapbox.com/',
+    'https://events.mapbox.com/',
+]
+const fontSrcUrls = []
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", 'blob:'],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                'blob:',
+                'data:',
+                'https://res.cloudinary.com/dtijo8xha/',
+                'https://images.unsplash.com/',
+                'https://source.unsplash.com/',
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+)
 
 // passport
 app.use(passport.initialize())
