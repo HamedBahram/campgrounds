6d067a9d538edd886975acf0375979efb62e86bf
HamedBahram <64147793+HamedBahram@users.noreply.github.com>
2021-02-16 21:10:05 -0500

    add favicon

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 const express = require('express')
 const path = require('path')
+const favicon = require('serve-favicon')
 const methodOverride = require('method-override')
 const AppError = require('./utils/AppError')
 const mongoose = require('mongoose')
@@ -42,6 +43,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 if (process.env.NODE_ENV !== 'production') {
